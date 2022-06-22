81110ce6e576d2aab92791279e020388699c2abb
Marco Viana <marcoviana100@gmail.com>
2022-06-21 22:03:46 -0300

    Exercicio 3 finalizado

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-3.js
@@ -0,0 +1,14 @@
+const num1 = 27;
+const num2 = 20;
+const num3 = 21;
+
+if (num1 > num2 && num1 > num3) {
+    console.log('O número 1 é o maior');
+}
+else if (num2 > num1 && num2 > num3) {
+    console.log('O número 2 é o maior');
+}
+
+else{
+    console.log('O número 3 é o maior');
+}
