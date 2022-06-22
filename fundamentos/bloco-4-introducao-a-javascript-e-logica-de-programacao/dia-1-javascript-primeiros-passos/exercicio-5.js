86d9c6eb73b1e65a08f72cc306141afbd64e9946
Marco Viana <marcoviana100@gmail.com>
2022-06-21 22:14:24 -0300

    Exercicio 5 finalizado

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-5.js
@@ -0,0 +1,14 @@
+const angulo1= 60;
+const angulo2= 62;
+const angulo3= 500;
+
+if (angulo1 + angulo2 + angulo3 === 180) {
+    console.log(true);
+}
+else if (angulo1 + angulo2 + angulo3 < 0) {
+    console.log('Erro');
+}
+else
+{
+    console.log(false);
+}
